cfe29242227c81000cf23ef902096f4203ecd4aa
ainoylaoutinen <bgp962@myy.haaga-helia.fi>
2021-09-23 09:51:28 +0300

    first commit

---
+++ bloodpressureapp/src/main/java/com/example/bloodpressureapp/BloodpressureappApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloodpressureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodpressureappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodpressureappApplication.class, args);
+	}
+
+}
